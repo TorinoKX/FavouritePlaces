af1ed56b962e4b2cdb3218c8413e0c19fcee9db4
Zak Cobham-Davis <zak.cobham-davis@griffithuni.edu.au>
2022-05-09 11:22:44 +1000

    Make edit mode functional in DetailView

--- FavouritePlaces/Views/DetailView.swift
+++ FavouritePlaces/Views/DetailView.swift
@@ -10,14 +10,40 @@ import SwiftUI
 struct DetailView: View {
     @Environment(\.editMode) var editMode
     @State var image = Image("Placeholder")
-    var location: Location
+    @ObservedObject var location: Location
     var body: some View {
         List {
             if editMode?.wrappedValue == .active {
-                
+                TextField("Enter Name of Location", text: $location.locName)
+                TextField("Enter Image URL", text: $location.urlString)
+                    .onSubmit {
+                        Task{
+                            image = await location.getImage()
+                        }
+                    }
+                VStack{
+                    Text("Enter Location Description:")
+                        .bold()
+                    TextEditor(text: $location.locDesc)
+                }
+                VStack {
+                    HStack{
+                        Text("Latitiude: ")
+                            .bold()
+                        TextField("", text: $location.lat)
+                    }
+                    HStack{
+                        Text("Longitude: ")
+                            .bold()
+                        TextField("", text: $location.long)
+                    }
+                }
             } else {
-                image.aspectRatio(contentMode: .fit)
-                    .frame(width: 32, height: 32)
+                HStack{
+                    Spacer()
+                    image.aspectRatio(contentMode: .fit)
+                    Spacer()
+                }
                 Text(location.locDesc)
                 Text("Latitude: \(location.lat) \nLongitude: \(location.long)")
             }
