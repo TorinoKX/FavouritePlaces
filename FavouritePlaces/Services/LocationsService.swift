f924915c8c01b057f94505d59be4d058f0ee8ed1
Zak Cobham-Davis <zak.cobham-davis@griffithuni.edu.au>
2022-05-18 20:56:54 +1000

    Extract the add and delete functions to a service file for tidiness

---
+++ FavouritePlaces/Services/LocationsService.swift
@@ -0,0 +1,41 @@
+//
+//  LocationsService.swift
+//  FavouritePlaces
+//
+//  Created by zak on 18/5/2022.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+class LocationsService {
+    func addItem(viewContext: NSManagedObjectContext) {
+        let newItem = Location(context: viewContext)
+        newItem.name = "New Place"
+        newItem.desc = ""
+        
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+    func deleteItems(offsets: IndexSet, viewContext: NSManagedObjectContext, locations: FetchedResults<Location>) {
+        offsets.map { locations[$0] }.forEach(viewContext.delete)
+        
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+    
+}
--- FavouritePlaces/Views/MasterView.swift
+++ FavouritePlaces/Views/MasterView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MasterView: View {
     @Environment(\.managedObjectContext) private var viewContext
+    var locationsService: LocationsService = LocationsService()
     var locations: FetchedResults<Location>
     
     var body: some View {
@@ -24,35 +25,11 @@ struct MasterView: View {
         .navigationTitle("Favourite Places")
     }
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Location(context: viewContext)
-            newItem.name = "New Place"
-            newItem.desc = ""
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    func addItem() {
+        locationsService.addItem(viewContext: viewContext)
     }
     
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { locations[$0] }.forEach(viewContext.delete)
-            
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    func deleteItems(index: IndexSet) {
+        locationsService.deleteItems(offsets: index, viewContext: viewContext, locations: locations)
     }
 }
